76905bc287b080a94984bc8e398719a101fb807c
Inna Isareva <inna8110302@gmail.com>
2023-09-29 11:28:20 +0300

    Added models ClothingType, Materials, Size, Clothing, Designer to models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ClothingType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Materials(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Clothing(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    clothing_type = models.ForeignKey(ClothingType, on_delete=models.CASCADE, related_name="clothes")
+    materials = models.ManyToManyField(Materials,  related_name="clothes")
+    size = models.ManyToManyField(Size, related_name="clothes")
+    designer = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="clothes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name} by {self.designer} - ${self.price}"
+
+    def get_absolute_url(self):
+        return reverse("catalog:clothing-detail", args=[str(self.id)])
+
+
+class Designer(AbstractUser):
+    pseudonym = models.CharField(max_length=255, blank=True, null=True, unique=True)
+
+    class Meta:
+        ordering = ['username']
+        verbose_name = "designer"
+        verbose_name_plural = "designers"
+
+    def __str__(self) -> str:
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("catalog:designer-detail", args=[str(self.id)])
