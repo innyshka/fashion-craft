de2cde0e30457b50d67c367e87a5cf3866345615
Inna Isareva <inna8110302@gmail.com>
2023-10-03 19:16:36 +0300

    Refactored urls.py

--- fashion_craft/urls.py
+++ fashion_craft/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from catalog.views import register
 
 urlpatterns = [
@@ -25,4 +26,9 @@ urlpatterns = [
     path("", include("catalog.urls", namespace="catalog")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", register, name="register"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
