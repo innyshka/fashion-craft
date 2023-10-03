4eaa4a1f0779e4e5e2fb1a3aa82856d7902d5585
Inna Isareva <inna8110302@gmail.com>
2023-10-03 18:18:01 +0300

    Refactored settings.py

--- fashion_craft/settings.py
+++ fashion_craft/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-MEDIA_ROOT = "/media/"
+# MEDIA_ROOT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
