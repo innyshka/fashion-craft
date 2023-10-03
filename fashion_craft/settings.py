6cb93cf906dde647f04e4a5ae17f04bfb9c07ffb
Inna Isareva <inna8110302@gmail.com>
2023-10-03 17:15:17 +0300

    Removed insecure Django secret key from settings.py

--- fashion_craft/settings.py
+++ fashion_craft/settings.py
@@ -23,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-!03n3+z9+zbl)j5_ykk6537x1hr@6570p%yb_efd4918kend%!"
-)
+# SECRET_KEY = os.environ.get(
+#     "DJANGO_SECRET_KEY",
+#     "django-insecure-!03n3+z9+zbl)j5_ykk6537x1hr@6570p%yb_efd4918kend%!"
+# )
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
